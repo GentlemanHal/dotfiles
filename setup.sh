939de14d02ea0e99590ad5dcad2601bdc7463f42
Christopher Martin <GentlemanHal@users.noreply.github.com>
2017-10-18 22:19:02 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,3 +2,6 @@
 
 # install Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# install Node Version Manager
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.5/install.sh | bash
