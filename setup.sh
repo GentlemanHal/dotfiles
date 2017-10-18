445b77dc1a0f58e7573f3603c3aaf2be605fe5b8
Christopher Martin <GentlemanHal@users.noreply.github.com>
2017-10-18 23:42:42 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,11 +7,16 @@ cp ./.bashrc ~/.bashrc
 # source the new bash profile
 . ~/.bash_profile
 
+# copy ssh configuration
+cp -R ./.ssh/. ~/.ssh
+
 # install Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# install tools and applications via Homebrew
 brew bundle
 
-# install Node Version Manager
+# install Node Version Manager (as installation via Homebrew is not recommended)
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.5/install.sh | bash
 
 # copy app configuration
